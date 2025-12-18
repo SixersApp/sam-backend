0127b50fbd1d798219441ae7b65394e32094d648
vabhi <vabhi2007@gmail.com>
2025-12-17 23:06:57 -0800

    Update app.ts

--- users-functions/app.ts
+++ users-functions/app.ts
@@ -49,7 +49,7 @@ app.put("/users/auth/signup", async (req, res) => {
 
   const userData = req.body;
 
-  const username = req.lambdaEvent.requestContext.authorizer?.claims?.["cognito:sub"];
+  const username = req.lambdaEvent.requestContext.authorizer?.claims?.["sub"];
 
   if (!username) {
     return res.status(401).json({ message: "Unauthorized" });
@@ -131,7 +131,7 @@ app.put("/users/auth/signup", async (req, res) => {
    ======================================================================================= */
 app.get("/users/profile", async (req, res) => {
   const userId =
-    req.lambdaEvent.requestContext.authorizer?.claims?.["cognito:sub"];
+    req.lambdaEvent.requestContext.authorizer?.claims?.["sub"];
 
   if (!userId) {
     return res.status(401).json({ message: "Unauthorized" });
